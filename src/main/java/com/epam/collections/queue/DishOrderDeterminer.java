4c817bbbcd5a836fff82024bc4b088302664cce0
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-07-26 15:00:12 +0500

    first and third tasks are solved

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,30 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> result = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+
+        int counter = 1;
+        while (!queue.isEmpty()) {
+            int dishNumber = queue.poll();
+            if (counter % everyDishNumberToEat == 0) {
+                result.add(dishNumber);
+            } else {
+                queue.add(dishNumber);
+            }
+            counter++;
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,20 @@
 package com.epam.collections.queue;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+
+        PriorityQueue<String> temp = new PriorityQueue<>( Comparator.reverseOrder());
+        PriorityQueue<String> result = new PriorityQueue<>( Comparator.reverseOrder());
+
+        temp.addAll(firstList);
+        temp.addAll(secondList);
+        while (!temp.isEmpty()) {
+            result.add(temp.poll());
+        }
+        return result;
     }
 }
